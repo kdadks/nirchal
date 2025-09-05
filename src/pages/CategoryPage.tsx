751088bab2f26c36d224f6ff1ca5b20a6bef7cf4
Amit <amit.ranjan78@gmail.com>
2025-09-05 14:16:56 +0530

    Removed UseEffect Error

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { ShoppingBag, ArrowRight, Filter, Grid, List } from 'lucide-react';
